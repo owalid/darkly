5772c63eaab2586f2445c8d7902ecb841cc23225
owalid <othmane.el.ayadi@gmail.com>
2022-11-15 19:15:30 +0100

    ok

--- route-search/download_dir_dict.sh
+++ route-search/download_dir_dict.sh
@@ -1,1 +1,1 @@
-curl -o dir-list-lowercase.txt "https://raw.githubusercontent.com/v0re/dirb/master/wordlists/big.txt"
+curl -o dir-list-lowercase.txt "https://raw.githubusercontent.com/3ndG4me/KaliLists/master/dirb/common.txt"
